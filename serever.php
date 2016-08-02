0d5abadf0603911d32858fc5dce883fcec900542
Bogdan <bogdan.derevyaga@gmail.com>
2016-08-02 17:31:52 +0300

    Update serever.php

--- serever.php
+++ serever.php
@@ -1,6 +1,7 @@
 <?php
 header('HTTP/1.1 201 Created');
 header("Content-Type: application/json;charset=utf-8");
+header('Access-Control-Allow-Origin: *');  
 
 $dsn= 'mysql:host=mysql.hostinger.com.ua;dbname=u919952063_test';
 $db_user = "***";
